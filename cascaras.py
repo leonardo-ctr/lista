5ca02776b3489286d269cdb1fcd2bea60d12708c
pollocandente <a.23308051281211@cbtis128.edu.mx>
2024-09-25 19:52:16 -0600

    Add files via upload

---
+++ cascaras.py
@@ -0,0 +1,34 @@
+print("Roman De la Cruz Leonardo Antonio, listas")
+thislist = ["apple", "banana", "cherry"]#las variables
+print(thislist)#imprime la lista
+
+#cuando usamos (len( )) te despliega
+#el numero de elementos de la lista
+thislist2 = ["apple", "banana", "cherry"]
+print(len(thislist))
+#Separa con un espacio
+print(" ")
+#Da valores a varias listas
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+list4 = ["abc", 34, True, 40, "male"]
+
+#Muestra los miembros de cada lista
+print(list1)
+print(list2)
+print(list3)
+print(list4)
+
+
+print(" ")
+
+thislist = ["apple", "banana", "cherry"]
+
+#Muestra el miembro 1 y comienza en Cero 0
+print(thislist[1])
+
+#Muestra el ultimo miembro de la lisa
+print(thislist[-1])
+
+print(" ")
